21ef374c6865dc9b2b5e030e517405b2de02c34e
adri <adri23458@gmail.com>
2022-11-28 14:59:39 +0100

    Acabamos gestion de ficheros con gestion de json y csv

--- Mouredev Intermedio/Gestion de ficheros.py
+++ Mouredev Intermedio/Gestion de ficheros.py
@@ -21,6 +21,31 @@ json_file = open('myFile.json', 'w+')
 json_test = {
     "name": 'Adrian',
     "country": "Spain",
-    "language": "Python",
+    "languages": ["Python", "JavaScript", "C++"],
     "example": "https://jason.org"}
-json.dump(json_test, json_file, indent=4)
+json.dump(json_test, json_file, indent=2 )
+json_file.close()
+with open('myFile.json', 'r') as my_other_file:
+    for line in my_other_file.readlines():
+        print(line[0:-1])
+json_dict = json.load(open('myFile.json'))  # Carga un diccionariode un fichero json a una variable
+print(json_dict)
+print(type(json_dict))
+
+
+# .csv file
+import csv
+
+csv_file = open('myFile.csv', 'w+')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(json_dict.keys())
+csv_writer.writerow(json_dict.values())
+csv_file.close()
+
+
+# .xlsx file
+#import xlrd #Modulo externo, se debe instalar a parte
+
+
+# .xml file
+import xml
